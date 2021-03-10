157962a7d18636eac9042f5d9250dd0722da1d0a
TelevizorFisher <>
2021-03-11 00:28:16 +0200

    Updates

---
+++ precache-manifest.e8457e5f804331668d67cb07a0bdb571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d442a31fbc3bb54361dc952adac05d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dec88c554bdb6e59ab1c",
+    "url": "/static/css/main.c4026e75.chunk.css"
+  },
+  {
+    "revision": "33ed1a780267e8c93e48",
+    "url": "/static/js/2.d6b004c5.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.d6b004c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec88c554bdb6e59ab1c",
+    "url": "/static/js/main.bc35cfb4.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "8ac9b6cf944c10d121750f15b33a1de9",
+    "url": "/static/media/logo.8ac9b6cf.jpg"
+  }
+]);
